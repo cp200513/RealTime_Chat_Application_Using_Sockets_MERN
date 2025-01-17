f2840cff5299b61cfd47a542cda03f01cb8f907a
cp200513 <v.v.chiranjeevprasannaa@gmail.com>
2025-01-17 17:48:16 +0530

    Backend Job Done

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { logout, signup, login } from "../controllers/auth.controller.js";
+import {
+  logout,
+  signup,
+  login,
+  updateProfile,
+  checkAuth,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,5 +15,9 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
