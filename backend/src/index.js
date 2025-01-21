837dd1664810d2902d19fd14f13410842d47a14b
cp200513 <v.v.chiranjeevprasannaa@gmail.com>
2025-01-21 21:59:11 +0530

    Final Commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,13 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,6 +24,13 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server Up! PORT : ${PORT}`);
