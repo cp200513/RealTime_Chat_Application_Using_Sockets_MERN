942fce947e8258b6479609c8767b2b7ee6b2ebce
cp200513 <v.v.chiranjeevprasannaa@gmail.com>
2025-01-21 22:54:01 +0530

    updated packages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,18 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
+
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
@@ -27,12 +30,13 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log(`Server Up! PORT : ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
