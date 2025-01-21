770ea8271ba153f95bed87c690e75d14086f33a1
cp200513 <v.v.chiranjeevprasannaa@gmail.com>
2025-01-21 23:03:55 +0530

    hope this is the last commit! :)

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
